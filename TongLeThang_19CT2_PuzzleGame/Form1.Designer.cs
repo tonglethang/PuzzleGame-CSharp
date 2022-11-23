a5c35d8c5d19224623d641e8217fee07de900294
unknown <thangle2444@gmail.com>
2022-11-23 17:32:29 +0700

    create

---
+++ TongLeThang_19CT2_PuzzleGame/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace TongLeThang_19CT2_PuzzleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbPuzzleBox = new System.Windows.Forms.GroupBox();
+            this.pbx1 = new System.Windows.Forms.PictureBox();
+            this.pbx2 = new System.Windows.Forms.PictureBox();
+            this.pbx3 = new System.Windows.Forms.PictureBox();
+            this.pbx4 = new System.Windows.Forms.PictureBox();
+            this.pbx5 = new System.Windows.Forms.PictureBox();
+            this.pbx6 = new System.Windows.Forms.PictureBox();
+            this.pbx7 = new System.Windows.Forms.PictureBox();
+            this.pbx8 = new System.Windows.Forms.PictureBox();
+            this.pbx9 = new System.Windows.Forms.PictureBox();
+            this.gbOriginal = new System.Windows.Forms.GroupBox();
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblMovesMade = new System.Windows.Forms.Label();
+            this.lblTimeElapsed = new System.Windows.Forms.Label();
+            this.tmrTimeElapse = new System.Windows.Forms.Timer(this.components);
+            this.btnPause = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboImg = new System.Windows.Forms.ComboBox();
+            this.gbPuzzleBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbPuzzleBox
+            // 
+            this.gbPuzzleBox.Controls.Add(this.pbx1);
+            this.gbPuzzleBox.Controls.Add(this.pbx2);
+            this.gbPuzzleBox.Controls.Add(this.pbx3);
+            this.gbPuzzleBox.Controls.Add(this.pbx4);
+            this.gbPuzzleBox.Controls.Add(this.pbx5);
+            this.gbPuzzleBox.Controls.Add(this.pbx6);
+            this.gbPuzzleBox.Controls.Add(this.pbx7);
+            this.gbPuzzleBox.Controls.Add(this.pbx8);
+            this.gbPuzzleBox.Controls.Add(this.pbx9);
+            this.gbPuzzleBox.Location = new System.Drawing.Point(483, 3);
+            this.gbPuzzleBox.Margin = new System.Windows.Forms.Padding(4);
+            this.gbPuzzleBox.Name = "gbPuzzleBox";
+            this.gbPuzzleBox.Padding = new System.Windows.Forms.Padding(4);
+            this.gbPuzzleBox.Size = new System.Drawing.Size(498, 533);
+            this.gbPuzzleBox.TabIndex = 0;
+            this.gbPuzzleBox.TabStop = false;
+            this.gbPuzzleBox.Text = "Xếp hình";
+            // 
+            // pbx1
+            // 
+            this.pbx1.Location = new System.Drawing.Point(20, 28);
+            this.pbx1.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx1.Name = "pbx1";
+            this.pbx1.Size = new System.Drawing.Size(151, 160);
+            this.pbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx1.TabIndex = 0;
+            this.pbx1.TabStop = false;
+            this.pbx1.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx2
+            // 
+            this.pbx2.Location = new System.Drawing.Point(174, 28);
+            this.pbx2.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx2.Name = "pbx2";
+            this.pbx2.Size = new System.Drawing.Size(151, 160);
+            this.pbx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx2.TabIndex = 0;
+            this.pbx2.TabStop = false;
+            this.pbx2.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx3
+            // 
+            this.pbx3.Location = new System.Drawing.Point(328, 28);
+            this.pbx3.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx3.Name = "pbx3";
+            this.pbx3.Size = new System.Drawing.Size(151, 160);
+            this.pbx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx3.TabIndex = 0;
+            this.pbx3.TabStop = false;
+            this.pbx3.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx4
+            // 
+            this.pbx4.Location = new System.Drawing.Point(20, 191);
+            this.pbx4.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx4.Name = "pbx4";
+            this.pbx4.Size = new System.Drawing.Size(151, 160);
+            this.pbx4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx4.TabIndex = 0;
+            this.pbx4.TabStop = false;
+            this.pbx4.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx5
+            // 
+            this.pbx5.Location = new System.Drawing.Point(174, 191);
+            this.pbx5.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx5.Name = "pbx5";
+            this.pbx5.Size = new System.Drawing.Size(151, 160);
+            this.pbx5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx5.TabIndex = 0;
+            this.pbx5.TabStop = false;
+            this.pbx5.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx6
+            // 
+            this.pbx6.Location = new System.Drawing.Point(328, 191);
+            this.pbx6.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx6.Name = "pbx6";
+            this.pbx6.Size = new System.Drawing.Size(151, 160);
+            this.pbx6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx6.TabIndex = 0;
+            this.pbx6.TabStop = false;
+            this.pbx6.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx7
+            // 
+            this.pbx7.Location = new System.Drawing.Point(20, 353);
+            this.pbx7.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx7.Name = "pbx7";
+            this.pbx7.Size = new System.Drawing.Size(151, 160);
+            this.pbx7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx7.TabIndex = 0;
+            this.pbx7.TabStop = false;
+            this.pbx7.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx8
+            // 
+            this.pbx8.Location = new System.Drawing.Point(174, 353);
+            this.pbx8.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx8.Name = "pbx8";
+            this.pbx8.Size = new System.Drawing.Size(151, 160);
+            this.pbx8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx8.TabIndex = 0;
+            this.pbx8.TabStop = false;
+            this.pbx8.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx9
+            // 
+            this.pbx9.Location = new System.Drawing.Point(328, 353);
+            this.pbx9.Margin = new System.Windows.Forms.Padding(4);
+            this.pbx9.Name = "pbx9";
+            this.pbx9.Size = new System.Drawing.Size(151, 160);
+            this.pbx9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx9.TabIndex = 0;
+            this.pbx9.TabStop = false;
+            this.pbx9.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // gbOriginal
+            // 
+            this.gbOriginal.BackgroundImage = global::TongLeThang_19CT2_PuzzleGame.Properties.Resources.dadia;
+            this.gbOriginal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gbOriginal.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gbOriginal.Location = new System.Drawing.Point(57, 13);
+            this.gbOriginal.Margin = new System.Windows.Forms.Padding(4);
+            this.gbOriginal.Name = "gbOriginal";
+            this.gbOriginal.Padding = new System.Windows.Forms.Padding(4);
+            this.gbOriginal.Size = new System.Drawing.Size(401, 356);
+            this.gbOriginal.TabIndex = 0;
+            this.gbOriginal.TabStop = false;
+            this.gbOriginal.Text = "Hình mẫu";
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.BackColor = System.Drawing.Color.White;
+            this.btnShuffle.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnShuffle.Location = new System.Drawing.Point(503, 544);
+            this.btnShuffle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(101, 52);
+            this.btnShuffle.TabIndex = 1;
+            this.btnShuffle.Text = "Trộn";
+            this.btnShuffle.UseVisualStyleBackColor = false;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.Color.White;
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnQuit.Location = new System.Drawing.Point(862, 544);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(101, 52);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Thoát";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // lblMovesMade
+            // 
+            this.lblMovesMade.AutoSize = true;
+            this.lblMovesMade.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovesMade.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblMovesMade.Location = new System.Drawing.Point(61, 534);
+            this.lblMovesMade.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMovesMade.Name = "lblMovesMade";
+            this.lblMovesMade.Size = new System.Drawing.Size(122, 24);
+            this.lblMovesMade.TabIndex = 2;
+            this.lblMovesMade.Text = "Số lần xếp: ";
+            // 
+            // lblTimeElapsed
+            // 
+            this.lblTimeElapsed.AutoSize = true;
+            this.lblTimeElapsed.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeElapsed.Location = new System.Drawing.Point(182, 459);
+            this.lblTimeElapsed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTimeElapsed.Name = "lblTimeElapsed";
+            this.lblTimeElapsed.Size = new System.Drawing.Size(125, 37);
+            this.lblTimeElapsed.TabIndex = 3;
+            this.lblTimeElapsed.Text = "00:00:00";
+            // 
+            // tmrTimeElapse
+            // 
+            this.tmrTimeElapse.Enabled = true;
+            this.tmrTimeElapse.Interval = 900;
+            this.tmrTimeElapse.Tick += new System.EventHandler(this.UpdateTimeElapsed);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(692, 544);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(101, 52);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.PauseOrResume);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(61, 471);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Thời gian: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(53, 405);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Chọn hình: ";
+            // 
+            // comboImg
+            // 
+            this.comboImg.AutoCompleteCustomSource.AddRange(new string[] {
+            "Ghềnh đá dĩa",
+            "Bãi biển"});
+            this.comboImg.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboImg.FormattingEnabled = true;
+            this.comboImg.Items.AddRange(new object[] {
+            "Dải ngân hà",
+            "Ghềnh đá dĩa",
+            "Bãi biển"});
+            this.comboImg.Location = new System.Drawing.Point(189, 402);
+            this.comboImg.Name = "comboImg";
+            this.comboImg.Size = new System.Drawing.Size(149, 31);
+            this.comboImg.TabIndex = 9;
+            this.comboImg.Text = "Dải ngân hà";
+            this.comboImg.SelectedIndexChanged += new System.EventHandler(this.comboImg_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 609);
+            this.Controls.Add(this.comboImg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.lblTimeElapsed);
+            this.Controls.Add(this.lblMovesMade);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.gbOriginal);
+            this.Controls.Add(this.gbPuzzleBox);
+            this.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Xếp Hình";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AskPermissionBeforeQuite);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbPuzzleBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbPuzzleBox;
+        private System.Windows.Forms.GroupBox gbOriginal;
+        private System.Windows.Forms.PictureBox pbx3;
+        private System.Windows.Forms.PictureBox pbx9;
+        private System.Windows.Forms.PictureBox pbx6;
+        private System.Windows.Forms.PictureBox pbx2;
+        private System.Windows.Forms.PictureBox pbx8;
+        private System.Windows.Forms.PictureBox pbx5;
+        private System.Windows.Forms.PictureBox pbx1;
+        private System.Windows.Forms.PictureBox pbx7;
+        private System.Windows.Forms.PictureBox pbx4;
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblMovesMade;
+        private System.Windows.Forms.Label lblTimeElapsed;
+        private System.Windows.Forms.Timer tmrTimeElapse;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboImg;
+    }
+}
+
---
+++ TongLeThang_19CT2_PuzzleGame/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TongLeThang_19CT2_PuzzleGame
+{
+    public partial class Form1 : Form
+    {
+        int inNullSliceIndex, inmoves = 0;
+        List<Bitmap> lstOriginalPictureList = new List<Bitmap>();
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+        private void comboImg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            object tmp = comboImg.Text.ToString();
+            if (tmp.Equals("Dải ngân hà"))
+            {
+                gbOriginal.BackgroundImage = Properties.Resources.nganha;
+                lstOriginalPictureList.AddRange(new Bitmap[] { Properties.Resources._1_1, Properties.Resources._1_2, Properties.Resources._1_3, Properties.Resources._1_4, Properties.Resources._1_5, Properties.Resources._1_6, Properties.Resources._1_7, Properties.Resources._1_8, Properties.Resources._1_9, Properties.Resources._null });
+                Shuffle();
+            }
+            else if (tmp.Equals("Bãi biển"))
+            {
+                gbOriginal.BackgroundImage = Properties.Resources.baibien;
+                lstOriginalPictureList.AddRange(new Bitmap[] { Properties.Resources._2_1, Properties.Resources._2_2, Properties.Resources._2_3, Properties.Resources._2_4, Properties.Resources._2_5, Properties.Resources._2_6, Properties.Resources._2_7, Properties.Resources._2_8, Properties.Resources._2_9, Properties.Resources._null });
+                Shuffle();
+            }
+            else if (tmp.Equals("Ghềnh đá dĩa"))
+            {
+                gbOriginal.BackgroundImage = Properties.Resources.dadia;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            gbOriginal.BackgroundImage = Properties.Resources.nganha;
+            lstOriginalPictureList.AddRange(new Bitmap[] { Properties.Resources._1_1, Properties.Resources._1_2, Properties.Resources._1_3, Properties.Resources._1_4, Properties.Resources._1_5, Properties.Resources._1_6, Properties.Resources._1_7, Properties.Resources._1_8, Properties.Resources._1_9, Properties.Resources._null });
+
+            lblMovesMade.Text += inmoves;
+            lblTimeElapsed.Text = "00:00:00";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        void Shuffle()
+        {
+            do
+            {
+                int j;
+                List<int> Indexes = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 9 });//8 is not present - since it is the last slice.
+                Random r = new Random();
+                for (int i = 0; i < 9; i++)
+                {
+                    Indexes.Remove((j = Indexes[r.Next(0, Indexes.Count)]));
+                    ((PictureBox)gbPuzzleBox.Controls[i]).Image = lstOriginalPictureList[j];
+                    if (j == 9) inNullSliceIndex = i;
+                }
+            } while (CheckWin());
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            DialogResult YesOrNo = new DialogResult();
+            if (lblTimeElapsed.Text != "00:00:00")
+            {
+                YesOrNo = MessageBox.Show("Bạn có muốn chơi lại ?", "Game xếp hình", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            if (YesOrNo == DialogResult.Yes || lblTimeElapsed.Text == "00:00:00")
+            {
+                Shuffle();
+                timer.Reset();
+                lblTimeElapsed.Text = "00:00:00";
+                inmoves = 0;
+                lblMovesMade.Text = "Số lần xếp: 0";
+            }
+        }
+
+        private void AskPermissionBeforeQuite(object sender, FormClosingEventArgs e)
+        {
+            DialogResult YesOrNO = MessageBox.Show("Bạn có muốn thoát ?", "Game xếp hình", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sender as Button != btnQuit && YesOrNO == DialogResult.No) e.Cancel = true;
+            if (sender as Button == btnQuit && YesOrNO == DialogResult.Yes) Environment.Exit(0);
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            AskPermissionBeforeQuite(sender, e as FormClosingEventArgs);
+        }
+
+        private void SwitchPictureBox(object sender, EventArgs e)
+        {
+            if (lblTimeElapsed.Text == "00:00:00")
+                timer.Start();
+            int inPictureBoxIndex = gbPuzzleBox.Controls.IndexOf(sender as Control);
+            if (inNullSliceIndex != inPictureBoxIndex)
+            {
+                List<int> arr = new List<int>(new int[] { ((inPictureBoxIndex % 3 == 0) ? -1 : inPictureBoxIndex - 1), inPictureBoxIndex - 3, (inPictureBoxIndex % 3 == 2) ? -1 : inPictureBoxIndex + 1, inPictureBoxIndex + 3 });
+                if (arr.Contains(inNullSliceIndex))
+                {
+                    ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image;
+                    ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image = lstOriginalPictureList[9];
+                    inNullSliceIndex = inPictureBoxIndex;
+                    lblMovesMade.Text = "Số lần xếp : " + (++inmoves);
+                    if (CheckWin())
+                    {
+                        timer.Stop();
+                        (gbPuzzleBox.Controls[8] as PictureBox).Image = lstOriginalPictureList[8];
+                        MessageBox.Show("Chúc mừng bạn...\nĐã xếp hình thành công !\nThời gian hoàn thành: " + timer.Elapsed.ToString().Remove(8) + "\nSố lần xếp: " + inmoves, "Game xếp hình");
+                        inmoves = 0;
+                        lblMovesMade.Text = "Số lần xếp : 0";
+                        lblTimeElapsed.Text = "00:00:00";
+                        timer.Reset();
+                        Shuffle();
+                    }
+                }
+            }
+        }
+
+        bool CheckWin()
+        {
+            int i;
+            for (i = 0; i < 8; i++)
+            {
+                if ((gbPuzzleBox.Controls[i] as PictureBox).Image != lstOriginalPictureList[i]) break;
+            }
+            if (i == 8) return true;
+            else return false;
+        }
+
+        private void UpdateTimeElapsed(object sender, EventArgs e)
+        {
+            if (timer.Elapsed.ToString() != "00:00:00")
+                lblTimeElapsed.Text = timer.Elapsed.ToString().Remove(8);
+            if (timer.Elapsed.ToString() == "00:00:00")
+                btnPause.Enabled = false;
+            else
+                btnPause.Enabled = true;
+            if (timer.Elapsed.Minutes.ToString() == "5")
+            {
+                timer.Reset();
+                lblMovesMade.Text = "Số lần xếp : 0";
+                lblTimeElapsed.Text = "00:00:00";
+                inmoves = 0;
+                btnPause.Enabled = false;
+                MessageBox.Show("Hết thời gian\nBạn có muốn thử lại", "Game xếp hình");
+                Shuffle();
+            }
+        }
+
+  
+
+        private void PauseOrResume(object sender, EventArgs e)
+        {
+            if (btnPause.Text == "Pause")
+            {
+                timer.Stop();
+                gbPuzzleBox.Visible = false;
+                btnPause.Text = "Resume";
+            }
+            else
+            {
+                timer.Start();
+                gbPuzzleBox.Visible = true;
+                btnPause.Text = "Pause";
+            }
+        }
+    }
+}
